1a734cedf01054fcf6b1c0836f659cae04aa861a
Piotr Kiełek <229910@edu.p.lodz.pl>
2024-01-14 19:14:12 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# restaurant_helper_tablet
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/restaurant_helper_tablet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.restaurant_helper_tablet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
